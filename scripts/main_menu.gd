b4a942fd7e6dcd501982c4b2d2e8d0df3687132f
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2025-12-29 23:54:33 +0800

    tilemapping

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
+@onready var start_level = preload("res://scenes/world.tscn")
 
 func _on_play_button_pressed() -> void:
-	#get_tree().change_scene_to_file()
-	pass # Replace with function body.
+	get_tree().change_scene_to_file(start_level)
 
 
 func _on_option_button_pressed() -> void:
